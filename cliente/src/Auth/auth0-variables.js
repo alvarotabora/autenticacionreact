37d03be3f1269049b83a9d29e91095f5b3abfef2
alvarotabora <41582433+alvarotabora@users.noreply.github.com>
2019-08-15 16:11:20 -0600

    Carga Inicial

---
+++ cliente/src/Auth/auth0-variables.js
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+  domain: 'dev-at.auth0.com',
+  clientId: 'Gs2SRPdCHfAobDjf1t4yTZIUT6QUkQ8l',
+  callbackUrl: 'http://localhost:3000/callback', 
+  apiURL: 'http://productos'
+}
